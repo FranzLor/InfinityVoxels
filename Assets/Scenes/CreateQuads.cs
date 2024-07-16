ab8c1cc2506edc16863df5844c072ab619282091
Franz <FranzLor64@yahoo.com>
2024-07-15 18:28:34 -0600

    Quads: Rendering Mesh of Quads
    updated and rendering quad meshes as a single cube

--- Assets/Scenes/CreateQuads.cs
+++ Assets/Scenes/CreateQuads.cs
@@ -4,12 +4,15 @@ using UnityEngine;
 
 public class CreateQuads : MonoBehaviour
 {
+
     public Material cubeMaterial;
 
-    void CreateQuad()
+    enum Cubeside { BOTTOM, TOP, LEFT, RIGHT, FRONT, BACK };
+
+    void CreateQuad(Cubeside side)
     {
         Mesh mesh = new Mesh();
-        mesh.name = "ScriptedMesh";
+        mesh.name = "ScriptedMesh" + side.ToString();
 
         Vector3[] vertices = new Vector3[4];
         Vector3[] normals = new Vector3[4];
@@ -22,7 +25,7 @@ public class CreateQuads : MonoBehaviour
         Vector2 uv01 = new Vector2(0f, 1f);
         Vector2 uv11 = new Vector2(1f, 1f);
 
-        //all possible vertices
+        //all possible vertices 
         Vector3 p0 = new Vector3(-0.5f, -0.5f, 0.5f);
         Vector3 p1 = new Vector3(0.5f, -0.5f, 0.5f);
         Vector3 p2 = new Vector3(0.5f, -0.5f, -0.5f);
@@ -32,15 +35,51 @@ public class CreateQuads : MonoBehaviour
         Vector3 p6 = new Vector3(0.5f, 0.5f, -0.5f);
         Vector3 p7 = new Vector3(-0.5f, 0.5f, -0.5f);
 
-
-        vertices = new Vector3[] { p4, p5, p1, p0 };
-        normals = new Vector3[] {Vector3.forward,
-                                 Vector3.forward,
-                                 Vector3.forward,
-                                 Vector3.forward};
-
-        uvs = new Vector2[] { uv11, uv01, uv00, uv10 };
-        triangles = new int[] { 3, 1, 0, 3, 2, 1 };
+        switch (side)
+        {
+            case Cubeside.BOTTOM:
+                vertices = new Vector3[] { p0, p1, p2, p3 };
+                normals = new Vector3[] {Vector3.down, Vector3.down,
+                                            Vector3.down, Vector3.down};
+                uvs = new Vector2[] { uv11, uv01, uv00, uv10 };
+                triangles = new int[] { 3, 1, 0, 3, 2, 1 };
+                break;
+            case Cubeside.TOP:
+                vertices = new Vector3[] { p7, p6, p5, p4 };
+                normals = new Vector3[] {Vector3.up, Vector3.up,
+                                            Vector3.up, Vector3.up};
+                uvs = new Vector2[] { uv11, uv01, uv00, uv10 };
+                triangles = new int[] { 3, 1, 0, 3, 2, 1 };
+                break;
+            case Cubeside.LEFT:
+                vertices = new Vector3[] { p7, p4, p0, p3 };
+                normals = new Vector3[] {Vector3.left, Vector3.left,
+                                            Vector3.left, Vector3.left};
+                uvs = new Vector2[] { uv11, uv01, uv00, uv10 };
+                triangles = new int[] { 3, 1, 0, 3, 2, 1 };
+                break;
+            case Cubeside.RIGHT:
+                vertices = new Vector3[] { p5, p6, p2, p1 };
+                normals = new Vector3[] {Vector3.right, Vector3.right,
+                                            Vector3.right, Vector3.right};
+                uvs = new Vector2[] { uv11, uv01, uv00, uv10 };
+                triangles = new int[] { 3, 1, 0, 3, 2, 1 };
+                break;
+            case Cubeside.FRONT:
+                vertices = new Vector3[] { p4, p5, p1, p0 };
+                normals = new Vector3[] {Vector3.forward, Vector3.forward,
+                                            Vector3.forward, Vector3.forward};
+                uvs = new Vector2[] { uv11, uv01, uv00, uv10 };
+                triangles = new int[] { 3, 1, 0, 3, 2, 1 };
+                break;
+            case Cubeside.BACK:
+                vertices = new Vector3[] { p6, p7, p3, p2 };
+                normals = new Vector3[] {Vector3.back, Vector3.back,
+                                            Vector3.back, Vector3.back};
+                uvs = new Vector2[] { uv11, uv01, uv00, uv10 };
+                triangles = new int[] { 3, 1, 0, 3, 2, 1 };
+                break;
+        }
 
         mesh.vertices = vertices;
         mesh.normals = normals;
@@ -49,7 +88,7 @@ public class CreateQuads : MonoBehaviour
 
         mesh.RecalculateBounds();
 
-        GameObject quad = new GameObject("quad");
+        GameObject quad = new GameObject("Quad");
         quad.transform.parent = this.gameObject.transform;
         MeshFilter meshFilter = (MeshFilter)quad.AddComponent(typeof(MeshFilter));
         meshFilter.mesh = mesh;
@@ -57,10 +96,53 @@ public class CreateQuads : MonoBehaviour
         renderer.material = cubeMaterial;
     }
 
+    void CombineQuads()
+    {
+        //combine all children meshes
+        MeshFilter[] meshFilters = GetComponentsInChildren<MeshFilter>();
+        CombineInstance[] combine = new CombineInstance[meshFilters.Length];
+        int i = 0;
+        while (i < meshFilters.Length)
+        {
+            combine[i].mesh = meshFilters[i].sharedMesh;
+            combine[i].transform = meshFilters[i].transform.localToWorldMatrix;
+            i++;
+        }
+
+        //creates a new mesh on the parent object
+        MeshFilter mf = (MeshFilter)this.gameObject.AddComponent(typeof(MeshFilter));
+        mf.mesh = new Mesh();
+
+        //adds combined meshes on children as the parent's mesh
+        mf.mesh.CombineMeshes(combine);
+
+        //create a renderer for the parent
+        MeshRenderer renderer = this.gameObject.AddComponent(typeof(MeshRenderer)) as MeshRenderer;
+        renderer.material = cubeMaterial;
+
+        //delete all uncombined children
+        foreach (Transform quad in this.transform)
+        {
+            Destroy(quad.gameObject);
+        }
+
+    }
+
+    void CreateCube()
+    {
+        CreateQuad(Cubeside.FRONT);
+        CreateQuad(Cubeside.BACK);
+        CreateQuad(Cubeside.TOP);
+        CreateQuad(Cubeside.BOTTOM);
+        CreateQuad(Cubeside.LEFT);
+        CreateQuad(Cubeside.RIGHT);
+        CombineQuads();
+    }
+
     // Use this for initialization
     void Start()
     {
-        CreateQuad();
+        CreateCube();
     }
 
     // Update is called once per frame
